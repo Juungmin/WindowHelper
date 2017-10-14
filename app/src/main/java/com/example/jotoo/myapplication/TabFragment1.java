feabc5f79fdbd010ec6eebc787b0e5c8bb19291e
hyunjeong <tlaguswjd2828@gmail.com>
2017-10-14 16:43:37 +0900

    Pager Adapter

---
+++ app/src/main/java/com/example/jotoo/myapplication/TabFragment1.java
@@ -0,0 +1,20 @@
+package com.example.jotoo.myapplication;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by kwangwoon on 2017. 10. 14..
+ */
+
+public class TabFragment1 extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_1, container, false);
+    }
+
+}
