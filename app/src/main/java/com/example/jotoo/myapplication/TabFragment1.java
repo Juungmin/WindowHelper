6b3b185d629e53c61d3938587d31f874e2931bb1
Eun Jeong <hej78520@gmail.com>
2017-10-16 02:59:19 +0900

    Connected to firebase

--- app/src/main/java/com/example/jotoo/myapplication/TabFragment1.java
+++ app/src/main/java/com/example/jotoo/myapplication/TabFragment1.java
@@ -3,9 +3,17 @@ package com.example.jotoo.myapplication;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by kwangwoon on 2017. 10. 14..
@@ -13,8 +21,46 @@ import android.view.ViewGroup;
 
 public class TabFragment1 extends Fragment {
 
+    FirebaseDatabase database;
+    DatabaseReference myRef_dust;
+
+    TextView tv_dust;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_1, container, false);
+
+        View view =  inflater.inflate(R.layout.tab_fragment_1, container, false);
+        //TextView tv_dust;
+
+        tv_dust = (TextView) view.findViewById(R.id.tv_dust);
+
+        database = FirebaseDatabase.getInstance();
+
+        myRef_dust = database.getReference("home test");
+        myRef_dust = myRef_dust.child("dust");
+        myRef_dust = myRef_dust.child("dust_val");
+        //Read from the DB
+        //update if there is a change on DB
+        myRef_dust.addValueEventListener(new ValueEventListener(){
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+
+                //String value = dataSnapshot.getValue(String.class);
+                String value = dataSnapshot.getValue(String.class);
+                //데이터를 화면에 출력해 준다.
+                tv_dust.setText("우리 집 미세먼지 농도: "+value + " ㎍/㎥");
+                //Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                //Log.w(TAG, "Failed to read value.", error.toException());
+            }
+
+        });
+        return view;
     }
 
 }
--- app/src/main/java/com/example/jotoo/myapplication/TabFragment2.java
+++ app/src/main/java/com/example/jotoo/myapplication/TabFragment2.java
@@ -9,11 +9,55 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class TabFragment2 extends Fragment {
 
+    FirebaseDatabase database;
+    DatabaseReference myRef_gas;
+
+    TextView tv_gas;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_2, container, false);
+        View view =  inflater.inflate(R.layout.tab_fragment_2, container, false);
+        //TextView tv_dust;
+
+        tv_gas = (TextView) view.findViewById(R.id.tv_gas);
+
+        database = FirebaseDatabase.getInstance();
+
+        myRef_gas = database.getReference("home test");
+        myRef_gas = myRef_gas.child("gas");
+        myRef_gas = myRef_gas.child("gas_val");
+        //Read from the DB
+        //update if there is a change on DB
+        myRef_gas.addValueEventListener(new ValueEventListener(){
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+
+                //String value = dataSnapshot.getValue(String.class);
+                String value = dataSnapshot.getValue(String.class);
+                //데이터를 화면에 출력해 준다.
+                tv_gas.setText("우리 집 가스 농도: "+value + " ppm");
+                //Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                //Log.w(TAG, "Failed to read value.", error.toException());
+            }
+
+        });
+        return view;
     }
 }
