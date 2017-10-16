6759fd41beba8452727e352abf0b9cacedc969fa
Eun Jeong <hej78520@gmail.com>
2017-10-17 01:26:52 +0900

    Add smoking picture on Tab2

--- app/src/main/java/com/example/jotoo/myapplication/TabFragment2.java
+++ app/src/main/java/com/example/jotoo/myapplication/TabFragment2.java
@@ -9,7 +9,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.*;
+import android.util.Log;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,10 +20,13 @@ import com.google.firebase.database.ValueEventListener;
 
 public class TabFragment2 extends Fragment {
 
+    private static final String TAG = "Tab2";
+
     FirebaseDatabase database;
     DatabaseReference myRef_gas;
 
     TextView tv_gas;
+    ImageView iv_smoke;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -30,15 +34,18 @@ public class TabFragment2 extends Fragment {
         //TextView tv_dust;
 
         tv_gas = (TextView) view.findViewById(R.id.tv_gas);
+        iv_smoke = (ImageView) view.findViewById(R.id.iv_smoke);
 
         database = FirebaseDatabase.getInstance();
 
         myRef_gas = database.getReference("home test");
         myRef_gas = myRef_gas.child("gas");
         myRef_gas = myRef_gas.child("gas_val");
+
         //Read from the DB
         //update if there is a change on DB
         myRef_gas.addValueEventListener(new ValueEventListener(){
+            double gas_val = 0;
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
@@ -49,6 +56,20 @@ public class TabFragment2 extends Fragment {
                 //데이터를 화면에 출력해 준다.
                 tv_gas.setText("우리 집 가스 농도: "+value + " ppm");
                 //Log.d(TAG, "Value is: " + value);
+
+                try {
+                    gas_val = Double.parseDouble(value);
+                    Log.d(TAG, "Value is: " + (int) gas_val);
+
+                    if((int) gas_val >= 200) {
+                        iv_smoke.setImageResource(R.drawable.smoking);
+                    }
+                    else {
+                        iv_smoke.setImageResource(R.drawable.no_smoking);
+                    }
+                } catch(NumberFormatException nfe) {
+                    System.out.println("Could not parse " + nfe);
+                }
             }
 
             @Override
