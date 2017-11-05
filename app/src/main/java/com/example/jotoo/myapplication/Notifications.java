b9d09d4d1562ec2019929cceeef9086e3093ef44
Juungmin <jotoomn@naver.com>
2017-11-05 18:11:13 +0900

    WindowHelper

--- app/src/main/java/com/example/jotoo/myapplication/Notifications.java
+++ app/src/main/java/com/example/jotoo/myapplication/Notifications.java
@@ -1,8 +1,38 @@
 package com.example.jotoo.myapplication;
 
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.TextView;
+
 /**
  * Created by jotoo on 2017-11-04.
  */
 
-public class Notifications {
+public class Notifications extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub1);
+        CharSequence s = "전달 받은 값은 ";
+        int id=0;
+
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) {
+            s = "error";
+        }
+        else {
+            id = extras.getInt("notificationId");
+        }
+        TextView t = (TextView) findViewById(R.id.text1);
+        s = s+" "+id;
+        t.setText(s);
+        NotificationManager nm =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //노티피케이션 제거
+        nm.cancel(id);
+    }
 }
