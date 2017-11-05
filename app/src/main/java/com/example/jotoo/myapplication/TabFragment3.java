0b1d37ff58273bf38e1f2923e084b72a1475c4a6
Juungmin <jotoomn@naver.com>
2017-11-05 22:45:35 +0900

    WindowHelper

--- app/src/main/java/com/example/jotoo/myapplication/TabFragment3.java
+++ app/src/main/java/com/example/jotoo/myapplication/TabFragment3.java
@@ -142,8 +142,6 @@ public class TabFragment3 extends Fragment {
                 .setWhen(System.currentTimeMillis())
                 .setDefaults(Notification.DEFAULT_ALL);
 
-
-
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
             builder.setCategory(Notification.CATEGORY_MESSAGE)
                     .setPriority(Notification.PRIORITY_HIGH)
